4d84658082bdcbebf3b50159085c3dd9d0f18d3b
dinhminh0307 <dinhminh181003@gmail.com>
2025-04-10 00:48:44 +0700

    chore: rm unused import

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import styles from './Navbar.module.css';
 import { useAppSelector, useAppDispatch } from '../../store/hooks';
 import { checkAuthState, logout } from '../../store/slices/authSlice';
